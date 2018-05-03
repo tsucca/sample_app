5abca4031430c1a4e8cb5e87822ebb5009c738d4
tsucca <user@gmail.com>
2018-05-03 14:13:11 +0000

    Finish ch05

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,5 +6,10 @@ class StaticPagesController < ApplicationController
   end
   
   def about
+    # 'app/views/static_pages/about.html.erb'
+  end
+  
+  def contact
+    # 'app/views/static_pages/contact.html.erb'
   end
 end
