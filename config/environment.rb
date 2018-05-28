56f1d9d9e3a498c202bc02c109ce5841d2139fa7
tsucca <user@gmail.com>
2018-05-28 12:55:35 +0000

    Finish CH13

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# NameError: uninitialized constant Micropost::PictureUploader エラー対処
+#require 'carrierwave/orm/activerecord'
