ec4764a75798531898928a1129d0f03eef3048f0
tsucca <user@gmail.com>
2018-06-03 14:18:32 +0000

    Finish ch14

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
+  #get 'password_resets/new'
 
-  get 'password_resets/edit'
+  #get 'password_resets/edit'
 
   #get 'users/new'
   root 'static_pages#home'
@@ -12,8 +12,13 @@ Rails.application.routes.draw do
   get  '/login',   to: 'sessions#new'
   post '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
